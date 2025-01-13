e81244b758a7d9334d6640475c58f58a060f1108
Technosuper <128301592+Technosuper@users.noreply.github.com>
2025-01-13 13:53:34 -0600

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

--- images.g.ts
+++ images.g.ts
@@ -4,23 +4,302 @@ namespace myImages {
     helpers._registerFactory("image", function(name: string) {
         switch(helpers.stringTrim(name)) {
             case "image1":
-            case "flaggy":return img`
-. . . . . 2 f . . . . . . . . . 
-. . . . 2 2 f . . . . . . . . . 
-. . . 2 2 2 2 . . . . . . . . . 
-. . . . 2 2 f . . . . . . . . . 
-. . . . . 2 f . . . . . . . . . 
-. . . . . . f . . . . . . . . . 
-. . . . . . f . . . . . . . . . 
-. . . . . . f . . . . . . . . . 
-. . . . . . f . . . . . . . . . 
-. . . . . . f . . . . . . . . . 
-. . . . . . f . . . . . . . . . 
-. . . . . . f . . . . . . . . . 
-. . . . . . f . . . . . . . . . 
-. . . . . . f . . . . . . . . . 
-. . . . . . f . . . . . . . . . 
+            case "Shop":return img`
+....................e2e22e2e....................
+.................222eee22e2e222.................
+..............222e22e2e22eee22e222..............
+...........e22e22eeee2e22e2eeee22e22e...........
+........eeee22e22e22e2e22e2e22e22e22eeee........
+.....222e22e22eeee22e2e22e2e22eeee22e22e222.....
+...22eeee22e22e22e22eee22eee22e22e22e22eeee22...
+4cc22e22e22eeee22e22e2e22e2e22e22eeee22e22e22cc4
+6c6eee22e22e22e22e22e2e22e2e22e22e22e22e22eee6c6
+46622e22eeee22e22eeee2e22e2eeee22e22eeee22e22664
+46622e22e22e22eeee22e2e22e2e22eeee22e22e22e22664
+4cc22eeee22e22e22e22eee22eee22e22e22e22eeee22cc4
+6c622e22e22eeee22e22e2e22e2e22e22eeee22e22e226c6
+466eee22e22e22e22e22e2e22e2e22e22e22e22e22eee664
+46622e22eeee22e22e22e2e22e2e22e22e22eeee22e22664
+4cc22e22e22e22e22eeee2e22e2eeee22e22e22e22e22cc4
+6c622eeee22e22eeee22eee22eee22eeee22e22eeee226c6
+46622e22e22eeee22e22e2e22e2e22e22eeee22e22e22664
+466eee22e22e22e22e22e2e22e2e22e22e22e22e22eee664
+4cc22e22eeee22e22e22e2e22e2e22e22e22eeee22e22cc4
+6c622e22e22e22e22e22eee22eee22e22e22e22e22e226c6
+46622eeee22e22e22eeecc6666cceee22e22e22eeee22664
+46622e22e22e22eeecc6666666666cceee22e22e22e22664
+4cceee22e22eeecc66666cccccc66666cceee22e22eeecc4
+6c622e22eeecc66666cc64444446cc66666cceee22e226c6
+46622e22cc66666cc64444444444446cc66666cc22e22664
+46622cc6666ccc64444444444444444446ccc6666cc22664
+4ccc6666ccc6444bcc666666666666ccb4446ccc6666ccc4
+cccccccc6666666cb44444444444444bc6666666cccccccc
+64444444444446c444444444444444444c64444444444446
+66cb444444444cb411111111111111114bc444444444bc66
+666cccccccccccd166666666666666661dccccccccccc666
+6666444444444c116eeeeeeeeeeeeee611c4444444446666
+666e2222222e4c16e4e44e44e44e44ee61c4e2222222e666
+666eeeeeeeee4c16e4e44e44e44e44ee61c4eeeeeeeee666
+666eddddddde4c66f4e4effffffe44ee66c4eddddddde666
+666edffdffde4c66f4effffffffff4ee66c4edffdffde666
+666edccdccde4c66f4effffffffffeee66c4edccdccde666
+666eddddddde4c66f4eeeeeeeeeeeeee66c4eddddddde666
+c66edffdffde4c66e4e44e44e44e44ee66c4edffdffde66c
+c66edccdccde4c66e4e44e44e44e44ee66c4edccdccde66c
+cc66666666664c66e4e44e44e44feeee66c46666666666cc
+.c66444444444c66e4e44e44e44ffffe66c44444444466c.
+..c64eee4eee4c66f4e44e44e44f44fe66c4eee4eee46c..
+...c4eee4eee4c66f4e44e44e44effee66c4eee4eee4c...
+....644444444c66f4e44e44e44e44ee66c444444446....
+.....64eee444c66f4e44e44e44e44ee66c444eee46.....
+......6ccc666c66e4e44e44e44e44ee66c666ccc6......
+`;
+            case "^UG(IOm_cyU0usO9wr0r":
+            case "Robin":return img`
+. . . . . . . . . . b 2 b . . . 
+. . . . . . . . . b 2 b . . . . 
+. . . . . . b b b b b b . . . . 
+. . . . . b b 2 2 2 2 2 b . . . 
+. . . . b b 2 d 1 f 2 d 4 c . . 
+. . . . b 2 2 1 f f d d 4 4 4 b 
+. . . . b 2 2 d f b 4 4 4 4 b . 
+. . . b d 2 2 2 2 4 4 4 4 b . . 
+. b b d d d 2 2 2 2 2 2 2 b . . 
+b d d d b b b 2 2 2 2 2 2 2 b . 
+c d d b 2 2 d c 2 2 2 2 2 2 b . 
+c b b d 2 d c d 2 2 2 2 2 2 b . 
+c b 2 2 b c d d 2 2 2 2 2 2 b . 
+b b c c c d d d 2 2 2 2 2 d b . 
+. . . . c c d d d 2 2 2 b b . . 
+. . . . . . c c c c c b b . . . 
+`;
+            case "awzi$*lkg=;{Q~RyBl%C":
+            case "Ducky":return img`
+. . . . . . . . . . b 5 b . . . 
+. . . . . . . . . b 5 b . . . . 
+. . . . . . b b b b b b . . . . 
+. . . . . b b 5 5 5 5 5 b . . . 
+. . . . b b 5 d 1 f 5 d 4 c . . 
+. . . . b 5 5 1 f f d d 4 4 4 b 
+. . . . b 5 5 d f b 4 4 4 4 b . 
+. . . b d 5 5 5 5 4 4 4 4 b . . 
+. b b d d d 5 5 5 5 5 5 5 b . . 
+b d d d b b b 5 5 5 5 5 5 5 b . 
+c d d b 5 5 d c 5 5 5 5 5 5 b . 
+c b b d 5 d c d 5 5 5 5 5 5 b . 
+c b 5 5 b c d d 5 5 5 5 5 5 b . 
+b b c c c d d d 5 5 5 5 5 d b . 
+. . . . c c d d d 5 5 5 b b . . 
+. . . . . . c c c c c b b . . . 
+`;
+            case "image2":
+            case "forestSnake0":return img`
+. . . . c c c c c c . . . . . . 
+. . . c 6 7 7 7 7 6 c . . . . . 
+. . c 7 7 7 7 7 7 7 7 c . . . . 
+. c 6 7 7 7 7 7 7 7 7 6 c . . . 
+. c 7 c 6 6 6 6 c 7 7 7 c . . . 
+. f 7 6 f 6 6 f 6 7 7 7 f . . . 
+. f 7 7 7 7 7 7 7 7 7 7 f . . . 
+. . f 7 7 7 7 6 c 7 7 6 f c . . 
+. . . f c c c c 7 7 6 f 7 7 c . 
+. . c 7 2 7 7 7 6 c f 7 7 7 7 c 
+. c 7 7 2 7 7 c f c 6 7 7 6 c c 
+c 1 1 1 1 7 6 f c c 6 6 6 c . . 
+f 1 1 1 1 1 6 6 c 6 6 6 6 f . . 
+f 6 1 1 1 1 1 6 6 6 6 6 c f . . 
+. f 6 1 1 1 1 1 1 6 6 6 f . . . 
+. . c c c c c c c c c f . . . . 
+`;
+            case "image3":
+            case "forestBat0":return img`
+. . f f f . . . . . . . . f f f 
+. f f c c . . . . . . f c b b c 
+f f c c . . . . . . f c b b c . 
+f c f c . . . . . . f b c c c . 
+f f f c c . c c . f c b b c c . 
+f f c 3 c c 3 c c f b c b b c . 
+f f b 3 b c 3 b c f b c c b c . 
+. c b b b b b b c b b c c c . . 
+. c 1 b b b 1 b b c c c c . . . 
+c b b b b b b b b b c c . . . . 
+c b c b b b c b b b b f . . . . 
+f b 1 f f f 1 b b b b f c . . . 
+f b b b b b b b b b b f c c . . 
+. f b b b b b b b b c f . . . . 
+. . f b b b b b b c f . . . . . 
 . . . f f f f f f f . . . . . . 
+`;
+            case "image4":
+            case "hermitCrabAttack0":return img`
+...................cc...
+...............cccc6ac..
+..............c6aaaa6c..
+..........cc.c6ccaaaaac.
+.........b22c6c22caaaaac
+.........ff2c6c2ffaaaaac
+.........ff2c6c2ff6aaacc
+.........b22aca22c6666cc
+..........b22a22caaaaaac
+.........cc22222bccaaaac
+........c2242224b22caac.
+........b24b4444bb2cbb..
+........c422b4b2224c42b.
+........c222c4c222c4c2c.
+........c2222c2222c4c2c.
+.........ccccccccc..ccc.
+`;
+            case "image5":
+            case "CAMOFish2":return img`
+. . . . . . . . . c c c c c . . 
+. . . . . . c c c 8 8 8 8 c c . 
+. . . . c c 8 8 8 8 8 8 8 8 c . 
+. . . . c 6 6 6 6 6 6 8 8 8 c . 
+. . . c 8 8 8 6 6 8 6 6 c c . . 
+. . . c 8 8 8 8 6 8 8 8 c . c c 
+. . . c 9 8 8 8 6 8 8 8 c 6 8 c 
+. . c c 9 8 9 8 6 8 8 8 6 6 8 c 
+c c c 9 9 8 8 8 6 8 6 8 8 8 8 c 
+f 9 9 9 8 8 8 8 8 8 6 8 6 6 c c 
+. f f 8 8 8 8 8 8 6 6 8 f . . . 
+. . . f 8 8 8 8 8 6 6 6 f . . . 
+. . . . f f 8 8 6 6 8 8 f . . . 
+. . . . . . f 8 8 8 8 8 f . . . 
+. . . . . . . f f f f f f . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image6":
+            case "CHOMPER":return img`
+. . . . . f c c c c f . . . . . 
+. . c c f b b 3 3 b b f c c . . 
+. c b 3 3 b b c c b b 3 3 b c . 
+. f 3 c c c b c c b c c c 3 f . 
+f c b b c c b c c b c c b b c f 
+c 3 c c b c c c c c c b c c 3 c 
+c 3 c c c f c c c c f c c c 3 c 
+. f b b c f f f c f f c b b f . 
+. . f b b c f 4 f f c b b f . . 
+. . c c c f 4 2 1 4 f c c c . . 
+. c 3 f f f 4 1 2 4 f f f 3 c . 
+c 3 f f f f 5 4 4 5 f f f f 3 c 
+f 3 c c f f f f f f f f c c 3 f 
+f b 3 c b b f b b f b b c 3 b f 
+. c b b 3 3 b 3 3 b 3 3 b b c . 
+. . f f f f f f f f f f f f . . 
+`;
+            case "image10":
+            case "mushroomFrontRight2":return img`
+. . . . . . . . . . . . . . . . 
+. . . . b b b b . . . . . . . . 
+. . . b 3 3 3 3 b b b b . . . . 
+. . b b 3 3 3 3 3 3 1 1 b c c . 
+. . b 3 3 3 3 3 3 1 1 1 3 c c c 
+. . b 1 1 3 3 3 3 3 3 3 3 3 b c 
+. . c 1 1 3 3 3 b c c c c b b f 
+. c c 3 3 3 b b d d d c c c b f 
+c b 3 3 b b d d d d d d b c b f 
+c 3 3 c b d d d d d d d d b c . 
+f 3 c c c d d d d d d c c d c . 
+f b c c c d d c c d d d d d f . 
+f b c c c d d d d d b b b d f . 
+f f b b c f f b d d d d d c . . 
+. f f f f d d b b d d d b f . . 
+. . . . f d d d b c c f f f . . 
+`;
+            case "image9":
+            case "kaijuMomAttack0":return img`
+........................
+........................
+..........ccc...........
+.........cccc...........
+.....ccccccc..ccc.......
+...cc777777cccccc.......
+..c7777777777bcc........
+.c777777777777b..cc.....
+c777772ff777777bccc.....
+c77d77ff77777777bc......
+c7777777777777777b......
+.cbb31bb7777dd777b.cc...
+.c7333b777ddddd77dccc...
+.c733b77ddddddddddb.....
+.c7777dddbb77bdddddccc..
+..ccccbbbb777bdddddccc..
+...cdcbc7777bddddddcc...
+....ccbc77bc7ddddddbcccc
+.....cbbcc7777dddddddddc
+.....ccbbb777ddbddddddc.
+.....cdcbc77ddbbbdddcc..
+...ccdddccddddbcbbcc....
+...ccccccd777ddccc......
+........cccccccc........
+`;
+            case "image7":
+            case "BOSS SHARK":return img`
+..............c222..............
+............cc22b2..............
+...........cb22b22.........222..
+..........fccbbc2.........c222..
+...fffffffcccccc22.......c22c...
+.ffcbbbbbbbbbbbbbcfff....c22f...
+fcbbbbbbbbbcbbbbbbcccff.c2bf....
+fbcbbbbffbbbcbcbbbcccccff2cf....
+fbb11112fbbbcbcbbbcccccc22cf....
+.fb11111111bbcbbbcccccccc22cf...
+..fccccccb11bbbbcccccccfff22f...
+...fc131c111bbbcccccbdbc..f222..
+....f33c111cbbccdddddbc....222..
+.....ff1111fdbbccddbcc..........
+.......cccccfdbbbfcc............
+.............fffff..............
+`;
+            case "image11":
+            case "shark0":return img`
+.............ccfff..............
+...........ccddbcf..............
+..........ccddbbf...............
+..........fccbbcf...............
+.....fffffccccccff.........ccc..
+...ffbbbbbbbcbbbbcfff....ccbbc..
+..fbbbbbbbbcbcbbbbcccff.cdbbc...
+ffbbbbbbffbbcbcbbbcccccfcdbbf...
+fbcbbb11ff1bcbbbbbcccccffbbf....
+fbbb11111111bbbbbcccccccbbcf....
+.fb11133cc11bbbbcccccccccccf....
+..fccc31c111bbbcccccbdbffbbcf...
+...fc13c111cbbbfcddddcc..fbbf...
+....fccc111fbdbbccdcc.....fbbf..
+........ccccfcdbbcc........fff..
+.............fffff..............
+`;
+            case "image12":
+            case "forestMonkey evil":return img`
+. . . . f f f f f . . . . . . . 
+. . . f e e e e e f . . . . . . 
+. . f f d d f e e e f . . . . . 
+. c d d f f d d e e f f . . . . 
+. c d f d d f d e e d d f . . . 
+c d d 2 d d 2 d e e b d c . . . 
+c d e e d d d d e e b d c . f f 
+c c c c c d d d e e f c . f e f 
+. f d d d d d e e f f . . f e f 
+. . f f f f f e e e e f . f e f 
+. . . . f e e e e e e e f f e f 
+. . . f e f f e f e e e e f f . 
+. . . f e f f e f e e e e f . . 
+. . . f d b f d b f f e f . . . 
+. . . f d d c d d b b d f . . . 
+. . . . f f f f f f f f f . . . 
+`;
+            case "image8":
+            case "coin1":return img`
+. . b b b . . . 
+. b 5 5 5 b . . 
+b 5 d 4 d 5 b . 
+b 5 4 5 1 5 b . 
+c 5 4 5 1 5 c . 
+c 5 5 1 5 5 c . 
+. f 5 5 5 f . . 
+. . f f f . . . 
 `;
         }
         return null;
@@ -28,93 +307,7 @@ namespace myImages {
 
     helpers._registerFactory("animation", function(name: string) {
         switch(helpers.stringTrim(name)) {
-            case "myAnim":
-            case "anim1":return [img`
-. . . . . . . f . . . . . . . . 
-. . . . . . . f . . . . . . . . 
-. . . . . . . f . . . . . . . . 
-. . . . . . . f . . . . . . . . 
-. . . . . . 2 f . . . . . . . . 
-. . . . . 2 2 f . . . . . . . . 
-. . . . 2 2 2 2 . . . . . . . . 
-. . . . . 2 2 f . . . . . . . . 
-. . . . . . 2 f . . . . . . . . 
-. . . . . . . f . . . . . . . . 
-. . . . . . . f . . . . . . . . 
-. . . . . . . f . . . . . . . . 
-. . . . . . . f . . . . . . . . 
-. . . . . . . f . . . . . . . . 
-. . . . . . . f . . . . . . . . 
-. . . . . f f f f f . . . . . . 
-`, img`
-. . . . . . . f . . . . . . . . 
-. . . . . . . f . . . . . . . . 
-. . . . . . . f . . . . . . . . 
-. . . . . . 2 f . . . . . . . . 
-. . . . . 2 2 f . . . . . . . . 
-. . . . 2 2 2 2 . . . . . . . . 
-. . . . . 2 2 f . . . . . . . . 
-. . . . . . 2 f . . . . . . . . 
-. . . . . . . f . . . . . . . . 
-. . . . . . . f . . . . . . . . 
-. . . . . . . f . . . . . . . . 
-. . . . . . . f . . . . . . . . 
-. . . . . . . f . . . . . . . . 
-. . . . . . . f . . . . . . . . 
-. . . . . . . f . . . . . . . . 
-. . . . . f f f f f . . . . . . 
-`, img`
-. . . . . . . f . . . . . . . . 
-. . . . . . 2 f . . . . . . . . 
-. . . . . 2 2 f . . . . . . . . 
-. . . . 2 2 2 2 . . . . . . . . 
-. . . . . 2 2 f . . . . . . . . 
-. . . . . . 2 f . . . . . . . . 
-. . . . . . . f . . . . . . . . 
-. . . . . . . f . . . . . . . . 
-. . . . . . . f . . . . . . . . 
-. . . . . . . f . . . . . . . . 
-. . . . . . . f . . . . . . . . 
-. . . . . . . f . . . . . . . . 
-. . . . . . . f . . . . . . . . 
-. . . . . . . f . . . . . . . . 
-. . . . . . . f . . . . . . . . 
-. . . . . f f f f f . . . . . . 
-`, img`
-. . . . . . 2 f . . . . . . . . 
-. . . . . 2 2 f . . . . . . . . 
-. . . . 2 2 2 2 . . . . . . . . 
-. . . . . 2 2 f . . . . . . . . 
-. . . . . . 2 f . . . . . . . . 
-. . . . . . . f . . . . . . . . 
-. . . . . . . f . . . . . . . . 
-. . . . . . . f . . . . . . . . 
-. . . . . . . f . . . . . . . . 
-. . . . . . . f . . . . . . . . 
-. . . . . . . f . . . . . . . . 
-. . . . . . . f . . . . . . . . 
-. . . . . . . f . . . . . . . . 
-. . . . . . . f . . . . . . . . 
-. . . . . . . f . . . . . . . . 
-. . . . . f f f f f . . . . . . 
-`, img`
-. . . . . . 2 f . . . . . . . . 
-. . . . . 2 2 f . . . . . . . . 
-. . . . 2 2 2 2 . . . . . . . . 
-. . . . . 2 2 f . . . . . . . . 
-. . . . . . 2 f . . . . . . . . 
-. . . . . . . f . . . . . . . . 
-. . . . . . . f . . . . . . . . 
-. . . . . . . f . . . . . . . . 
-. . . . . . . f . . . . . . . . 
-. . . . . . . f . . . . . . . . 
-. . . . . . . f . . . . . . . . 
-. . . . . . . f . . . . . . . . 
-. . . . . . . f . . . . . . . . 
-. . . . . . . f . . . . . . . . 
-. . . . . . . f . . . . . . . . 
-. . . . . f f f f f . . . . . . 
-`];
+
         }
         return null;
     })
--- main.ts
+++ main.ts
@@ -1,63 +1,33 @@
 namespace SpriteKind {
     export const Flag = SpriteKind.create()
 }
-scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile1`, function (sprite, location) {
-    game.gameOver(false)
-    game.setGameOverEffect(false, effects.clouds)
-    game.reset()
-})
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     if (Birdy_1_duck.vy == 0) {
         Birdy_1_duck.vy = -170
     }
 })
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.hazardLava0, function (sprite, location) {
+    game.gameOver(false)
+    game.setGameOverEffect(false, effects.clouds)
+    game.reset()
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.hazardLava1, function (sprite, location) {
+    game.gameOver(false)
+    game.setGameOverEffect(false, effects.clouds)
+    game.reset()
+})
 let Birdy_1_duck: Sprite = null
 scene.setBackgroundColor(9)
-tiles.setCurrentTilemap(tilemap`level4`)
+tiles.setCurrentTilemap(tilemap`level 1 two paths`)
 Birdy_1_duck = mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.One))
 let Birdy_2_robin = mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two))
 tiles.placeOnTile(Birdy_1_duck, tiles.getTileLocation(1, 6))
-Birdy_1_duck = sprites.create(img`
-    . . . . . . . . . . b 5 b . . . 
-    . . . . . . . . . b 5 b . . . . 
-    . . . . . . b b b b b b . . . . 
-    . . . . . b b 5 5 5 5 5 b . . . 
-    . . . . b b 5 d 1 f 5 d 4 c . . 
-    . . . . b 5 5 1 f f d d 4 4 4 b 
-    . . . . b 5 5 d f b 4 4 4 4 b . 
-    . . . b d 5 5 5 5 4 4 4 4 b . . 
-    . b b d d d 5 5 5 5 5 5 5 b . . 
-    b d d d b b b 5 5 5 5 5 5 5 b . 
-    c d d b 5 5 d c 5 5 5 5 5 5 b . 
-    c b b d 5 d c d 5 5 5 5 5 5 b . 
-    c b 5 5 b c d d 5 5 5 5 5 5 b . 
-    b b c c c d d d 5 5 5 5 5 d b . 
-    . . . . c c d d d 5 5 5 b b . . 
-    . . . . . . c c c c c b b . . . 
-    `, SpriteKind.Player)
+Birdy_1_duck = sprites.create(assets.image`Ducky`, SpriteKind.Player)
 splitScreen.cameraFollowSprite(splitScreen.Camera.Camera1, Birdy_1_duck)
 tiles.placeOnTile(Birdy_2_robin, tiles.getTileLocation(1, 6))
-Birdy_2_robin = sprites.create(img`
-    . . . . . . . . . . b 2 b . . . 
-    . . . . . . . . . b 2 b . . . . 
-    . . . . . . b b b b b b . . . . 
-    . . . . . b b 2 2 2 2 2 b . . . 
-    . . . . b b 2 d 1 f 2 d 4 c . . 
-    . . . . b 2 2 1 f f d d 4 4 4 b 
-    . . . . b 2 2 d f b 4 4 4 4 b . 
-    . . . b d 2 2 2 2 4 4 4 4 b . . 
-    . b b d d d 2 2 2 2 2 2 2 b . . 
-    b d d d b b b 2 2 2 2 2 2 2 b . 
-    c d d b 2 2 d c 2 2 2 2 2 2 b . 
-    c b b d 2 d c d 2 2 2 2 2 2 b . 
-    c b 2 2 b c d d 2 2 2 2 2 2 b . 
-    b b c c c d d d 2 2 2 2 2 d b . 
-    . . . . c c d d d 2 2 2 b b . . 
-    . . . . . . c c c c c b b . . . 
-    `, SpriteKind.Player)
+Birdy_2_robin = sprites.create(assets.image`Robin`, SpriteKind.Player)
 splitScreen.cameraFollowSprite(splitScreen.Camera.Camera2, Birdy_2_robin)
 Birdy_1_duck.ay = 200
 Birdy_2_robin.ay = 200
 controller.moveSprite(Birdy_1_duck, 100, 0)
 controller.player2.moveSprite(Birdy_2_robin, 100, 0)
-mp.setPlayerIndicatorsVisible(true)
